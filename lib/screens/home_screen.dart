0e21d8bea47582929598197511a29762cfa1ee72
Wickramaarachchi B S <132426591+WickramaarachchiBS@users.noreply.github.com>
2025-03-12 14:18:10 +0530

    appbar and somethings

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 1.0),
+            child: Icon(Icons.menu),
+          ),
+        ],
+      ),
+      body: SearchAnchor(
+        builder: (BuildContext context, SearchController controller,) {
+          return SearchBar(
+            controller: controller,
+            padding: const WidgetStatePropertyAll<EdgeInsets>(
+              EdgeInsets.symmetric(horizontal: 16.0),
+            ),
+            onTap: () {
+              controller.openView();
+            },
+            onChanged: (_) {
+              controller.openView();
+            },
+            leading: const Icon(Icons.search),
+          );
+        },
+      ),
+    );
+  }
+}
