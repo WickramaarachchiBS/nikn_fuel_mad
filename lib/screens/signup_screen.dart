b61212a0c1b59aea85ee9686abd6799ab511233e
Wickramaarachchi B S <132426591+WickramaarachchiBS@users.noreply.github.com>
2025-03-16 17:40:36 +0530

    nothing

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -9,14 +9,16 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   final _formKey = GlobalKey<FormState>();
+  String _username = '';
   String _email = '';
   String _password = '';
-  String _confirmPassword = '';
 
   void _submit() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      // Perform signup logic here
+      print('Username: $_username');
+      print('Email: $_email');
+      print('Password: $_password');
     }
   }
 
@@ -32,6 +34,18 @@ class _SignUpScreenState extends State<SignUpScreen> {
           key: _formKey,
           child: Column(
             children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _username = value!;
+                },
+              ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Email'),
                 keyboardType: TextInputType.emailAddress,
@@ -56,19 +70,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 },
                 onSaved: (value) {
                   _password = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Confirm Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value != _password) {
-                    return 'Passwords do not match';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _confirmPassword = value!;
                 },
               ),
               SizedBox(height: 20),
