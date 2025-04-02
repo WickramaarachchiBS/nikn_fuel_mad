da3670d4e1ec9d8f76efe1af7d6eb79a0a7b1930
Wickramaarachchi B S <132426591+WickramaarachchiBS@users.noreply.github.com>
2025-04-02 19:45:27 +0530

    title renamed in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'RefuelX',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
